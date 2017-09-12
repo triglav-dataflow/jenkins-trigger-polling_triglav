97745df6943cd11ebf715e3eca4f27ac3a088ad0
Jonatan Alama <jonatan.alama@dena.jp>
2017-09-12 16:15:17 +0900

    fix style

--- src/main/java/io/github/triglav_dataflow/jenkins/trigger/polling_triglav/TimeUnit.java
+++ src/main/java/io/github/triglav_dataflow/jenkins/trigger/polling_triglav/TimeUnit.java
@@ -1,16 +1,20 @@
 package io.github.triglav_dataflow.jenkins.trigger.polling_triglav;
 
-public enum TimeUnit {
-  HOURLY("hourly"),
-  DAILY("daily"),
-  SINGULAR("singular");
-  private String timeUnit;
-  private TimeUnit(String timeUnit)
-  {
-    this.timeUnit = timeUnit;
-  }
-  public String getValue()
-  {
-    return timeUnit;
-  }
+public enum TimeUnit
+{
+    HOURLY("hourly"),
+    DAILY("daily"),
+    SINGULAR("singular");
+
+    private String timeUnit;
+
+    private TimeUnit(String timeUnit)
+    {
+        this.timeUnit = timeUnit;
+    }
+
+    public String getValue()
+    {
+        return timeUnit;
+    }
 }
